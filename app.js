fe646bf041597e48a65fe48b0e0178e1c52235c4
Querido, Abraham <Abraham.Querido@ellucian.com>
2023-06-13 11:38:03 -0600

    added helmet, cors, xss, and rateLimiter

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const app = express();
 
 const authRouter = require("./routes/auth");
@@ -16,7 +21,18 @@ const authenticateUser = require("./middleware/authentication");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 // extra packages
 
 // routes
